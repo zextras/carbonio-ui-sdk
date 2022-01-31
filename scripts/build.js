300053344006822f7de9d2711d54744a37563395
Matteo Dal Zovo <zovomat@gmail.com>
2022-01-31 21:24:47 +0100

    feat: improved build logging

--- scripts/build.js
+++ scripts/build.js
@@ -8,15 +8,17 @@
 const arg = require('arg');
 const chalk = require('chalk');
 const webpack = require('webpack');
-const { buildSetup } = require('./utils/setup');
-const { pkg } = require('./utils/pkg');
-const { setupWebpackBuildConfig } = require('./configs/webpack.build.config');
+const { buildSetup } = require('@zextras/carbonio-ui-sdk/scripts/utils/setup');
+const { pkg } = require('@zextras/carbonio-ui-sdk/scripts/utils/pkg');
+const { setupWebpackBuildConfig } = require('@zextras/carbonio-ui-sdk/scripts/configs/webpack.build.config');
 
 function parseArguments() {
 	const args = arg(
 		{
 			'--analyze': Boolean,
-			'-a': '--analyze'
+			'-a': '--analyze',
+			'--verbose': Boolean,
+			'-v': '--verbose'
 		},
 		{
 			argv: process.argv.slice(2),
@@ -24,34 +26,46 @@ function parseArguments() {
 		}
 	);
 	return {
-		analyzeBundle: args['--analyze'] || false
+		analyzeBundle: args['--analyze'] || false,
+		verbose: args['--verbose'] || false
 	};
 }
 
+const logErrors = (errors, gravity, verbose) => {
+	const title = gravity === 'error' ? chalk.bgRed.white : chalk.bgYellow.white;
+	const message = gravity === 'error' ? chalk.redBright : chalk.yellowBright;
+	errors.forEach((error, i) => {
+		console.log(title(`${i + 1}/${errors.length}:`));
+		console.log(message(` > ${error.message}`));
+		if (error.moduleName) console.log(message('Module: '), error.moduleName);
+		if (error.file) console.log(message('File: '), error.file);
+		if (error.loc) console.log(message('Path: '), error.loc);
+		if (error.details) console.log(message('Details: '), error.details);
+		if (error.stack && verbose) console.log(message('Stack: '), error.stack);
+	});
+}
+
 const logBuild =
-	([resolve, reject]) =>
+	([resolve, reject], options) =>
 	(err, stats) => {
 		if (err) {
 			console.log(chalk.bgRed.white.bold('Webpack Runtime Error'));
-			console.error(err.stack || err);
-			if (err.details) {
-				console.error(err.details);
-			}
+			logErrors([err], 'error', options.verbose);
 			reject(err);
 		}
 
 		const info = stats.toJson();
 
 		if (stats.hasWarnings()) {
-			chalk.bgRed.white.bold(`Webpack Compilations Warning${info.warnings.length > 0 ? 's' : ''}`);
-			console.warn(info.warnings);
+			console.log(chalk.bgYellow.white.bold(`Webpack Compilations Warning${info.warnings.length > 1 ? 's' : ''}`));
+			logErrors(info.warnings, 'warning', options.verbose);
 		}
 
 		if (stats.hasErrors()) {
 			console.log(
-				chalk.bgRed.white.bold(`Webpack Compilations Error${info.errors.length > 0 ? 's' : ''}`)
+				chalk.bgRed.white.bold(`Webpack Compilations Error${info.errors.length > 1 ? 's' : ''}`)
 			);
-			console.error(info.errors);
+			logErrors(info.errors, 'error', options.verbose);
 			reject(err);
 		} else {
 			console.log(chalk.bgBlue.white.bold('Compiled Successfully!'));
@@ -66,5 +80,5 @@ exports.runBuild = () =>
 		console.log('Using base path ', chalk.green(buildSetup.basePath));
 		const config = setupWebpackBuildConfig(options, buildSetup);
 		const compiler = webpack(config);
-		compiler.run(logBuild(p));
+		compiler.run(logBuild(p, options));
 	});
