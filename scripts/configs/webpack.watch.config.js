5d9ec04b0d1b3bcdde20ed2f801181e776420091
Matteo Dal Zovo <matteo.dalzovo@zextras.com>
2022-03-29 12:12:38 +0200

    fix: malformed watch config

--- scripts/configs/webpack.watch.config.js
+++ scripts/configs/webpack.watch.config.js
@@ -18,91 +18,89 @@ exports.setupWebpackWatchConfig = (options, {basePath, commitHash}) => {
 	defaultConfig.output.filename = '[name].bundle.js'
 	defaultConfig.output.chunkFilename = '[name].chunk.js'
 	defaultConfig.devServer = {
-		devServer: {
-			hot: true,
-			port: 9000,
-			historyApiFallback: {
-				index: basePath
-				// TODO: remove once confirmed that it is not needed
-				// rewrites: { from: '/static/iris/carbonio-shell-ui/current', to: `${basePath}/index.html` }
-			},
-			server: 'https',
-			onBeforeSetupMiddleware(devServer) {
-				devServer.app.get('/_cli', (req, res) => {
-					res.json({
-						isWatch: true,
-						isStandalone: !!options.standalone,
-						server: server,
-						app_package: {
-							package: options.name,
-							name: options.name,
-							version: pkg.version,
-							description: pkg.description
-						}
-					});
-				});
-			},
-			open: [`/${pkg.carbonio.type}/`],
-			proxy: [
-				{
-					context: [`!${basePath}/**/*`, '!/static/iris/components.json'],
-					target: server,
-					secure: false,
-					logLevel: 'debug',
-					ws: true,
-					cookieDomainRewrite: {
-						'*': server,
-						[server]: 'localhost:9000'
-					}
-				},
-				{
-					context: ['/static/iris/components.json'],
-					target: server,
-					secure: false,
-					logLevel: 'debug',
-					cookieDomainRewrite: {
-						'*': server,
-						[server]: 'localhost:9000'
-					},
-					selfHandleResponse: false,
-					onProxyRes(proxyRes, req, res) {
-						modifyResponse(res, proxyRes, function (body) {
-							if (body?.components) {
-								console.log(chalk.green.bold('[Proxy] modifying components.json'));
-								let found = false;
-								const components = body.components.reduce((acc, module) => {
-									if (module.name === options.name) {
-										found = true;
-										return [...acc, {...module, js_entrypoint: `${basePath}app.bundle.js`}];
-									}
-									if (options.standalone)
-										return acc;
-									return [...acc, module];
-								}, []);
-								if (!found) {
-									components.push({
-										js_entrypoint: `${basePath}app.bundle.js`,
-										commit: commitHash,
-										description: pkg.description,
-										name: options.name,
-										priority: pkg.carbonio.priority,
-										version: pkg.version,
-										type: pkg.carbonio.type,
-										attrKey: pkg.carbonio.attrKey,
-										icon: pkg.carbonio.icon,
-										display: pkg.carbonio.display,
-										sentryDsn: pkg.carbonio.sentryDsn
-									})
-								}
-								return JSON.stringify({ components });
-							}
-							console.log(chalk.green.bold('[Proxy] components.json: no content'));
-							return body;
-						});
-					}
-				}
-			]
+		hot: true,
+		port: 9000,
+		historyApiFallback: {
+			index: basePath
+			// TODO: remove once confirmed that it is not needed
+			// rewrites: { from: '/static/iris/carbonio-shell-ui/current', to: `${basePath}/index.html` }
 		},
+		server: 'https',
+		onBeforeSetupMiddleware(devServer) {
+			devServer.app.get('/_cli', (req, res) => {
+				res.json({
+					isWatch: true,
+					isStandalone: !!options.standalone,
+					server: server,
+					app_package: {
+						package: options.name,
+						name: options.name,
+						version: pkg.version,
+						description: pkg.description
+					}
+				});
+			});
+		},
+		open: [`/${pkg.carbonio.type}/`],
+		proxy: [
+			{
+				context: [`!${basePath}/**/*`, '!/static/iris/components.json'],
+				target: server,
+				secure: false,
+				logLevel: 'debug',
+				ws: true,
+				cookieDomainRewrite: {
+					'*': server,
+					[server]: 'localhost:9000'
+				}
+			},
+			{
+				context: ['/static/iris/components.json'],
+				target: server,
+				secure: false,
+				logLevel: 'debug',
+				cookieDomainRewrite: {
+					'*': server,
+					[server]: 'localhost:9000'
+				},
+				selfHandleResponse: false,
+				onProxyRes(proxyRes, req, res) {
+					modifyResponse(res, proxyRes, function (body) {
+						if (body?.components) {
+							console.log(chalk.green.bold('[Proxy] modifying components.json'));
+							let found = false;
+							const components = body.components.reduce((acc, module) => {
+								if (module.name === options.name) {
+									found = true;
+									return [...acc, {...module, js_entrypoint: `${basePath}app.bundle.js`}];
+								}
+								if (options.standalone)
+									return acc;
+								return [...acc, module];
+							}, []);
+							if (!found) {
+								components.push({
+									js_entrypoint: `${basePath}app.bundle.js`,
+									commit: commitHash,
+									description: pkg.description,
+									name: options.name,
+									priority: pkg.carbonio.priority,
+									version: pkg.version,
+									type: pkg.carbonio.type,
+									attrKey: pkg.carbonio.attrKey,
+									icon: pkg.carbonio.icon,
+									display: pkg.carbonio.display,
+									sentryDsn: pkg.carbonio.sentryDsn
+								})
+							}
+							return JSON.stringify({ components });
+						}
+						console.log(chalk.green.bold('[Proxy] components.json: no content'));
+						return body;
+					});
+				}
+			}
+		]
 	}
 
 	const confPath = path.resolve(process.cwd(), 'carbonio.webpack.js');
