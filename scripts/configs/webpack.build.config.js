0ac600ea590302b5f1710fd5f87aa678630b70c4
Matteo Dal Zovo <zovomat@gmail.com>
2022-04-13 23:26:35 +0200

    feat: added shim for the preview package

--- scripts/configs/webpack.build.config.js
+++ scripts/configs/webpack.build.config.js
@@ -183,6 +183,7 @@ exports.setupWebpackBuildConfig = (options, { basePath, commitHash }, skipCustom
 		moment: `__ZAPP_SHARED_LIBRARIES__['moment']`,
 		'styled-components': `__ZAPP_SHARED_LIBRARIES__['styled-components']`,
 		'@reduxjs/toolkit': `__ZAPP_SHARED_LIBRARIES__['@reduxjs/toolkit']`,
+		'@zextras/carbonio-ui-preview': `__ZAPP_SHARED_LIBRARIES__['@zextras/carbonio-ui-preview']`,
 		'@zextras/carbonio-shell-ui': `__ZAPP_SHARED_LIBRARIES__['@zextras/carbonio-shell-ui']['${options.name}']`,
 		/* Exports for App's Handlers */
 		msw: `__ZAPP_SHARED_LIBRARIES__['msw']`
