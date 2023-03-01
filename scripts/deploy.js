a9f1c705f1cded458169b339716284c32991fe0b
Giuliano <75381134+giuliano176@users.noreply.github.com>
2023-03-01 09:39:33 +0100

    Add port option to deploy (#13)
    * feat: update deploy to work with podman
    
    * chore: clean up code
    
    * chore: clean up code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,12 +5,12 @@
  */
 
 /* eslint-disable no-console */
-const { execSync } = require("child_process");
-const chalkTemplate = require("chalk");
-const { handler: build, builder: buildOptions } = require("./build");
-const { pkg } = require("./utils/pkg");
-const { commitHash } = require("./utils/setup");
-const { printArgs } = require("./utils/console");
+const { execSync } = require('child_process');
+const chalkTemplate = require('chalk');
+const { handler: build, builder: buildOptions } = require('./build');
+const { pkg } = require('./utils/pkg');
+const { commitHash } = require('./utils/setup');
+const { printArgs } = require('./utils/console');
 
 const updateJson = (appJson, carbonioJson, options) => {
   const components = carbonioJson.components.filter(
@@ -20,62 +20,76 @@ const updateJson = (appJson, carbonioJson, options) => {
   return { components };
 };
 
-exports.command = "deploy";
-exports.desc = "Build and inject the project to a Carbonio instance";
+exports.command = 'deploy';
+exports.desc = 'Build and inject the project to a Carbonio instance';
 exports.builder = Object.assign(
   {
     host: {
-      desc: "Destination hostname",
+      desc: 'Destination hostname',
       demandOption: true,
-      alias: "h",
+      alias: 'h',
     },
     user: {
-      desc: "Username for ssh access",
-      alias: "u",
-      default: "root",
+      desc: 'Username for ssh access',
+      alias: 'u',
+      default: 'root',
+    },
+    port: {
+      desc: 'Localhost port to use',
+      alias: 'p',
+      default: '',
     },
   },
   buildOptions
 );
 
 exports.handler = async (options) => {
-  const pathPrefix = `/opt/zextras/${options.admin ? "admin" : "web"}/iris/`;
-  printArgs(options, "Deploy");
+  const pathPrefix = `/opt/zextras/${options.admin ? 'admin' : 'web'}/iris/`;
+  printArgs(options, 'Deploy');
   await build(options);
   if (options.host) {
-    const target = `${options.user}@${options.host}`;
-    console.log(`- Deploying to ${chalkTemplate.bold(target)}...`);
+    const cpTarget = `${options.user}@${options.host}`;
+    const sshTarget = `${options.user}@${options.host}${
+      options.port && ' -p'
+    } ${options.port}`;
+    console.log(`- Deploying to ${chalkTemplate.bold(sshTarget)}...`);
     execSync(
-      `ssh ${target} "cd ${pathPrefix} && rm -rf ${options.name}/* && mkdir -p ${options.name}/${commitHash} ${options.name}/current"`
+      `ssh ${sshTarget} "cd ${pathPrefix} && rm -rf ${options.name}/* && mkdir -p ${options.name}/${commitHash} ${options.name}/current"`
     );
     execSync(
-      `scp -r dist/* ${target}:${pathPrefix}${options.name}/${commitHash}`
+      `scp -r ${options.port && '-P'} ${
+        options.port
+      } dist/* ${cpTarget}:${pathPrefix}${options.name}/${commitHash}`
     );
-    console.log(`- Updating ${chalkTemplate.bold("components.json")}...`);
+    console.log(`- Updating ${chalkTemplate.bold('components.json')}...`);
     const components = JSON.stringify(
       updateJson(
         JSON.parse(
           execSync(
-            `ssh ${target} cat ${pathPrefix}${options.name}/${commitHash}/component.json`
+            `ssh ${sshTarget} cat ${pathPrefix}${options.name}/${commitHash}/component.json`
           ).toString()
         ),
         JSON.parse(
-          execSync(`ssh ${target} cat ${pathPrefix}components.json`).toString()
+          execSync(
+            `ssh ${sshTarget} cat ${pathPrefix}components.json`
+          ).toString()
         ),
         options
       )
     ).replace(/"/g, '\\"');
     execSync(
-      `ssh ${target} "echo '${components}' > ${pathPrefix}components.json"`
+      `ssh ${sshTarget} "echo '${components}' > ${pathPrefix}components.json"`
     );
     console.log(`- Updating html indexes...`);
     execSync(
-      `ssh ${target} "cd ${pathPrefix}${options.name}/${commitHash} && find . -name \"*.html\" -exec cp --parents \"{}\" ${pathPrefix}${options.name}/current/ \\;"`
+      `ssh ${sshTarget} "cd ${pathPrefix}${options.name}/${commitHash} && find . -name \"*.html\" -exec cp --parents \"{}\" ${pathPrefix}${options.name}/current/ \\;"`
     );
-    console.log(chalkTemplate.bgBlue.white.bold("Deploy Completed"));
+    console.log(chalkTemplate.bgBlue.white.bold('Deploy Completed'));
   } else {
     console.log(
-      chalkTemplate.bgYellow.white("Target host not specified, skipping deploy step")
+      chalkTemplate.bgYellow.white(
+        'Target host not specified, skipping deploy step'
+      )
     );
   }
 };
